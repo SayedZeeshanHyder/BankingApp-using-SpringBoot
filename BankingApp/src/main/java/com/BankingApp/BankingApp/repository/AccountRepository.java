53dcf02e7e242b75e7da5fb63c590896a38b60b3
SayedZeeshanHyder <zhyder133@gmail.com>
2024-07-28 18:51:08 +0530

    Scrapped the Codes to start again

--- BankingApp/src/main/java/com/BankingApp/BankingApp/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.BankingApp.BankingApp.repository;
-
-import com.BankingApp.BankingApp.model.AccountModel;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository<AccountModel, ObjectId> {
-}
